bd1b17d363bf8a6e76d026d2ea7dfacb0aae46e5
Golnaz8 <golnaz.brj@gmail.com>
2023-08-08 12:49:28 -0400

    added fsUtils and changed notes.js to debug

--- routes/notes.js
+++ routes/notes.js
@@ -1,65 +1,35 @@
 const notes = require('express').Router();
+const { readAndAppend, readFromFile } = require('../helpers/fsUtils');
 
+// GET Route for retrieving all the notes
+notes.get('/', (req, res) =>
+  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
+);
 
-
-// GET request for notes
-notes.get('/', (req, res) => {
-    // Send a message to the client
-    res.status(200).json('./db/db.json');
-
-    // Log our request to the terminal
-    console.info(`${req.method} request received to get reviews`);
-});
-
-// POST request to add a note
+// POST Route for submitting notes
 notes.post('/', (req, res) => {
-    // Log that a POST request was received
-    console.info(`${req.method} request received to add a review`);
+  // Destructuring assignment for the items in req.body
+  const { title, text } = req.body;
 
-    // Destructuring assignment for the items in req.body
-    const { title, text } = req.body;
+  // If all the required properties are present
+  if (title && text) {
+    // Variable for the object we will save
+    const newNotes = {
+       title,
+       text,
+    };
 
-    // If all the required properties are present
-    if (title && text) {
-        // Variable for the object we will save
-        const newNote = {
-            title,
-            text,
-        };
+    readAndAppend(newNotes, './db/db.json');
 
-        // Obtain existing notes
-        fs.readFile('./db/db.json', 'utf8', (err, data) => {
-            if (err) {
-                console.error(err);
-            } else {
-                // Convert string into JSON object
-                const parsedNotes = JSON.parse(data);
+    const response = {
+      status: 'success',
+      body: newNotes,
+    };
 
-                // Add a new note
-                parsedNotes.push(newNote);
-
-                // Write updated notes back to the file
-                fs.writeFile(
-                    './db/db.json',
-                    JSON.stringify(parsedNotes, null, 2),
-                    (writeErr) =>
-                        writeErr
-                            ? console.error(writeErr)
-                            : console.info('Successfully updated reviews!')
-                );
-            }
-        });
-
-        const response = {
-            status: 'success',
-            body: newNote,
-        };
-
-        console.log(response);
-        res.status(201).json(response);
-    } else {
-        res.status(500).json('Error in posting review');
-    }
+    res.json(response);
+  } else {
+    res.json('Error in posting feedback');
+  }
 });
 
 module.exports = notes;
