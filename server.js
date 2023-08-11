06285358152488786d768c03264938cb9aa798da
Golnaz8 <golnaz.brj@gmail.com>
2023-08-11 12:02:23 -0400

    added README file

--- server.js
+++ server.js
@@ -13,14 +13,14 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/api', api);
 
 
-// GET Route for homepage
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
-);
-
 // GET Route for notes page
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+// GET Route for homepage
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
 app.listen(PORT, () =>
